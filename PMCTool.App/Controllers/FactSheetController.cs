fb89f46b980758429a1c35081abb272b2076a116
a-carlin <acarlin@inoventi.com.mx>
2021-05-07 12:09:56 -0500

    Vista Ficha tecnica

---
+++ PMCTool.App/Controllers/FactSheetController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using PMCTool.App.Attributes;
+using PMCTool.App.Models;
+
+namespace PMCTool.App.Controllers
+{
+    
+    public class FactSheetController : BaseController
+    {
+        private readonly IOptions<AppSettingsModel> _appSettings;
+        public FactSheetController(
+            IOptions<AppSettingsModel> appSettings, 
+            IStringLocalizer<SharedResource> localizer) : base(appSettings, localizer)
+        {
+            _appSettings = appSettings;
+        }
+
+        //[PMCToolAuthorize(ObjectCode = "3100")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
