aa604cafbf6cd15abc7b57a016a0909e13ee4a64
Daniel Padilla <dan.pd@live.com>
2021-05-19 10:56:00 -0500

    mejoras

---
+++ PMCTool.App/wwwroot/js/plugins/analytics.js
@@ -0,0 +1,11 @@
+﻿const getCookieValue = (name) => (
+    document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
+)
+let gaEnvieroment = getCookieValue('pmctool-name-env');
+gaEnvieroment = gaEnvieroment.replace("%20", ""); 
+ga('send', {
+    hitType: 'event',
+    eventCategory: gaEnvieroment,
+    eventAction: 'Visita',
+    eventLabel: window.location.pathname
+});
