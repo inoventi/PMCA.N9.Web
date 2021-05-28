129e0002f82d27540286f19a4c81b74985cd057f
Daniel Padilla <dan.pd@live.com>
2021-05-28 13:31:03 -0500

    PMCToolAuthorize usarios ccp full access

--- PMCTool.App/Attributes/PMCToolAuthorizeAttribute.cs
+++ PMCTool.App/Attributes/PMCToolAuthorizeAttribute.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Routing;
 using Newtonsoft.Json;
 using PMCTool.Common.RestConnector;
 using PMCTool.Models.Core;
+using PMCTool.Models.Enumeration;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -36,12 +37,29 @@ namespace PMCTool.App.Attributes
             {
                 string token = context.HttpContext.Request.Cookies["pmctool-token-app"];
                 header.Add("Authorization", token);
-
+                //
+                var handler = new JwtSecurityTokenHandler();
+                var tokenString = handler.ReadJwtToken(token);
+                var userId = tokenString.Claims.FirstOrDefault(x => x.Type == "jti").Value;
+                var envId = tokenString.Claims.FirstOrDefault(x => x.Type == "Env").Value;
+                var profileUser = Task.Run<User>(() => restClient.Get<User>(baseUrl, $"/api/v1/Users/{userId}", new Dictionary<string, string>() { { "Authorization", token } })).Result;
+                ///
                 try
                 {
-                    var response = Task.Run<OkObjectResult>(() => restClient.Get<OkObjectResult>(baseUrl, $"/api/v1/useraccess/{ObjectCode}", header)).Result;
-                    var result = JsonConvert.DeserializeObject<UserObjectAccess>(response.Value.ToString());
-                    isValid = result.ValidAccess;
+
+                    if (profileUser.Type == (int)EnumFactory.UserType.App)
+                    {
+                        var response = Task.Run<OkObjectResult>(() => restClient.Get<OkObjectResult>(baseUrl, $"/api/v1/useraccess/{ObjectCode}", header)).Result;
+                        var result = JsonConvert.DeserializeObject<UserObjectAccess>(response.Value.ToString());
+                        isValid = result.ValidAccess;
+                    }
+                    else if (profileUser.Type == (int)EnumFactory.UserType.CCP) {
+                        isValid = true;
+                    }
+                    else{
+
+                    }
+                    
                 }
                 catch (Exception ex)
                 {
