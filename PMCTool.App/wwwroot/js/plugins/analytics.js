1f99679eb8bfa4b5e2d45139581f7e52a9717c3e
Daniel Padilla <dan.pd@live.com>
2021-06-28 12:20:03 -0500

    Ajuste de obtimizacion de analytics

--- PMCTool.App/wwwroot/js/plugins/analytics.js
+++ PMCTool.App/wwwroot/js/plugins/analytics.js
@@ -1,11 +1,19 @@
-﻿const getCookieValue = (name) => (
-    document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
-)
-let gaEnvieroment = getCookieValue('pmctool-name-env');
-gaEnvieroment = gaEnvieroment.replace("%20", ""); 
-ga('send', {
-    hitType: 'event',
-    eventCategory: gaEnvieroment,
-    eventAction: 'Visita',
-    eventLabel: window.location.pathname
-});
+﻿document.addEventListener("DOMContentLoaded", function () {  
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-124338594-1', 'auto');
+    ga('send', 'pageview'); 
+    let ienv = "[ANALYTICS] " + $('meta[name="client"]').attr("content");
+    ga('send', {
+        hitType: 'event',
+        eventCategory: ienv,
+        eventAction: 'Visita',
+        eventLabel: window.location.pathname
+    }); 
+
+}); 
