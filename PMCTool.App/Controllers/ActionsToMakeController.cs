ff98759ccdb53704fa3a455fed7d1ae2b430260b
mcardenasg <mcardenas@inoventi.com.mx>
2021-07-13 17:05:47 -0500

    reportes  Ubicacion de proyectos, Acciones a realizar

---
+++ PMCTool.App/Controllers/ActionsToMakeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PMCTool.App.Attributes;
+
+namespace PMCTool.App.Controllers
+{
+    public class ActionsToMakeController : Controller
+    {
+        [PMCToolAuthentication]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
