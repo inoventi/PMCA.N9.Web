2ee154f8b43585e61453bfb695e65fa98c477315
a-carlin <acarlin@inoventi.com.mx>
2021-07-20 09:55:43 -0500

    ABALYTICS SCT - Global Status

---
+++ PMCTool.App/Controllers/GlobalStatusController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PMCTool.App.Attributes;
+using PMCTool.App.Models;
+using PMCTool.Models.Environment;
+using PMCTool.Common.RestConnector;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Localization;
+
+namespace PMCTool.App.Controllers
+{
+    public class GlobalStatusController : BaseController
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public GlobalStatusController(IOptions<AppSettingsModel> appSettings, IStringLocalizer<SharedResource> localizer, IHostingEnvironment hostingEnvironment) : base(appSettings, localizer)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [PMCToolAuthentication]
+        public async Task<IActionResult> GetDataGlobalStatus(ModelFilters data)
+        {
+            List<GlobalStatus> globalStatusData = new List<GlobalStatus>();
+            try
+            {
+                globalStatusData = await restClient.Get<List<GlobalStatus>>(baseUrl,
+                                   $"api/v1/globalstatus/data?states={data.States}&generaldirection={data.GeneralDirection}&projecttype={data.ProjectType}&stage={data.Stage}&investment={data.Investment}&advertisement={data.Advertisement}",
+                   new Dictionary<string, string>() { { "Authorization", GetTokenValue("Token") } });
+                
+                return PartialView("_PartialTableGlobalStatus", globalStatusData);
+
+            }
+            catch (HttpResponseException ex)
+            {
+                return Json(new { hasError = true, message = ex.Message });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { hasError = true, message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [PMCToolAuthentication]
+        public async Task<IActionResult> GetDataGlobalStatusDetail(ModelFilters data)
+        {
+            List<GlobalStatusDetail> globalStatusDetailData = new List<GlobalStatusDetail>();
+
+            try
+            {
+                globalStatusDetailData = await restClient.Get<List<GlobalStatusDetail>>(baseUrl,
+                                    $"api/v1/globalstatus/detail/data?states={data.States}&generaldirection={data.GeneralDirection}&projecttype={data.ProjectType}&stage={data.Stage}&investment={data.Investment}&advertisement={data.Advertisement}",
+                   new Dictionary<string, string>() { { "Authorization", GetTokenValue("Token") } });
+
+                return PartialView("_PartialTableGlobalStatusDetail", globalStatusDetailData);
+
+            }
+            catch (HttpResponseException ex)
+            {
+                return Json(new { hasError = true, message = ex.Message });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { hasError = true, message = ex.Message });
+            }
+            
+        }
+    }
+}
