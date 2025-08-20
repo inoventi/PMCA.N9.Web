3d60d68cb52ecedc4401463e1b72d0a6fa1bcd91
Manuel Cardenas <manuel.cardenas@indar.com.mx>
2025-08-20 17:15:34 -0600

    Terminado ReportGetProjectSheetByID
    Se termino de construir la primera tabla con el sp de ReportGetProjectSheetById

--- PMCTool.App/wwwroot/js/core/analitycs/vProjectSheet.js
+++ PMCTool.App/wwwroot/js/core/analitycs/vProjectSheet.js
@@ -8,8 +8,18 @@
         });
     }
     async requesDataReportProjectSheet() {
-        LoaderShow();
+        
         let projectId = $('#selprojects').val();
+        if (projectId.length == 0) {
+            Swal.fire({
+                type: 'error',
+                title: '',
+                text: $.i18n._("Analytics5_024"),
+                footer: ''
+            });
+            return;
+        }
+        LoaderShow();
         let resquest = await fetch(`/ActionsToMake/ReportGetProjectSheetByID?projectId=${projectId}`);
         let { amountMinOfContract, client, endDateProject, idProjectClient, leader, maxContractAmount, priority, projectDimension, projectName, projectOverview, projetManager, startDateProject } = await resquest.json();
         this.resetRows();
@@ -17,14 +27,18 @@
         $('#clientRow').html(client);
         $('#leaderRow').html(leader);
         $('#idProjectClientRow').html(idProjectClient);
-        $('#startDateProjectRow').html(startDateProject);
-        $('#amountMinOfContractRow').html(amountMinOfContract);
+        $('#startDateProjectRow').html(startDateProject != null ? startDateProject.split('T')[0] : '');
+        $('#amountMinOfContractRow').html(`$${(amountMinOfContract == "" || amountMinOfContract == null) ? 0 : amountMinOfContract}`);
 
         $('#priorityRow').html(priority);
         $('#projectManagerRow').html(projetManager);
         $('#projectDimensionRow').html(projectDimension);
-        $('#endDateProjectRow').html(endDateProject);
-        $('#maxContractAmountRow').html(maxContractAmount);
+        $('#endDateProjectRow').html(endDateProject != null ? endDateProject.split('T')[0] : '');
+        $('#maxContractAmountRow').html(`$${(maxContractAmount == "" || maxContractAmount == null) ? 0 : maxContractAmount}`);
+
+
+        $('#projectOverviewRow').html(projectOverview);
+        $('#report').removeAttr('hidden');
 
         LoaderHide();
     }
@@ -40,6 +54,8 @@
         $('#projectDimensionRow').empty();
         $('#endDateProjectRow').empty();
         $('#maxContractAmountRow').empty();
+
+        $('#projectOverviewRow').empty();
     }
 }
 
